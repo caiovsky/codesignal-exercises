bd19ab6d78ecf569016c1e921867f75b5036f4a4
caiovsky <82966790+caiovsky@users.noreply.github.com>
2023-08-03 11:15:39 -0300

    solution firstDuplicate changeded from 'for' to 'foreach'

--- interview-practice/arrays/firstDuplicate.php
+++ interview-practice/arrays/firstDuplicate.php
@@ -4,49 +4,31 @@
  * Exercise: Interview Practice - Arrays - firstDuplicate
  * ===============================================
  *  Exercise link: https://app.codesignal.com/interview-practice/question/pMvymcahZ8dY4g75q
- *  My objective: Generate understandable code and solve the exercise.
+ *  My objective: Generate clean and understandable code to solve the exercise.
  *  PHP Version: 8.0.27
  * ===============================================
  */
 
 /**
- * Codesignal's main answer function.
- * @param array $a
- * @return int
+ * Find the first duplicate in the array and return it. If there are no duplicates, return -1.
+ * @param array $arr The input array of integers.
+ * @return int The first duplicate element, or -1 if no duplicates are found.
  */
-function solution(array $a): int {
-    // used SplFixedArray since its more optimized than array, but array can be used instead
-    $array = new SplFixedArray(count($a));
-    $array = $a;
-
-    // $aux and $pos are used in conjunction to generate the array [$aux] which $answer and $minIndex will be iterated
-    $aux = [];
-    $pos = [];
-
-    // $answer is the function's return value, $minIndex starts at infinity (INF constant)
+function solution(array $arr): int {
+    $elementPositions = [];
     $answer = -1;
     $minIndex = INF;
 
-    /**
-     * feeds $aux with values from $array,
-     * array_keys() is a viable solution, but I believe it's less optimized
-     */
-    for($i = 0; $i < count($array); $i++) {
-        if(!array_key_exists($array[$i], $aux)) {
-            $aux[$array[$i]][0] = $i;
-            $pos[$array[$i]] = 0;
-        }
-        else {
-            $aux[$array[$i]][$pos[$array[$i]]] = $i;
-        }
-        $pos[$array[$i]] += 1;
+    // Iterate through the input array to find duplicates and track their positions
+    foreach ($arr as $index => $value) {
+        $elementPositions[$value][] = $index;
     }
 
-    // iterates through $aux and searches for the minimal index
-    for($i = 1; $i < count($array)+1; $i++) {
-        if(isset($aux[$i][1]) && $aux[$i][1] < $minIndex) {
-            $minIndex = $aux[$i][1];
-            $answer = $i;
+    // Find the first duplicate and update the answer and minimum index accordingly
+    foreach ($elementPositions as $value => $positions) {
+        if (count($positions) > 1 && $positions[1] < $minIndex) {
+            $minIndex = $positions[1];
+            $answer = $value;
         }
     }
 
